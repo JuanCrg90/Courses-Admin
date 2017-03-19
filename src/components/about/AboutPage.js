ea083f801fea3df04b4e4fff5263aa4ed80ec5f5
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2017-03-19 01:01:48 -0600

    Add initial App Structure

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This applications uses React, Redux and a variety of other helpul tools</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
